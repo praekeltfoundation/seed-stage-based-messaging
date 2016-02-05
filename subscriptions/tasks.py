8b40bbbe18bcc0f520dcfaeae3cea69238c70ee4
gsvr <george@westerncapelabs.com>
2016-02-05 18:31:16 +0200

    conceptual scheduler move to api call

--- subscriptions/tasks.py
+++ subscriptions/tasks.py
@@ -11,7 +11,6 @@ from django.core.exceptions import ObjectDoesNotExist
 logger = get_task_logger(__name__)
 
 from .models import Subscription
-from mama_ng_control.scheduler.client import SchedulerApiClient  # TODO
 from contentstore.models import Schedule, MessageSet, Message
 
 
@@ -25,12 +24,6 @@ class Schedule_Create(Task):
 
         """ The attempted task failed because of a non-200 HTTP return code.
         """
-
-    def scheduler_client(self):  # TODO
-        return SchedulerApiClient(
-            username=settings.SCHEDULER_USERNAME,
-            password=settings.SCHEDULER_PASSWORD,
-            api_url=settings.SCHEDULER_URL)
 
     def schedule_to_cron(self, schedule):
         return "%s %s %s %s %s" % (
@@ -49,25 +42,31 @@ class Schedule_Create(Task):
         l.info("Creating schedule for <%s>" % (subscription_id,))
         try:
             subscription = Subscription.objects.get(id=subscription_id)
-            scheduler = self.scheduler_client()  # TODO
             # get the subscription schedule/protocol from content store
             l.info("Loading contentstore schedule <%s>" % (
                 subscription.schedule,))
-            csschedule = Schedule.objects.get(pk=subscription.schedule)
+            cs_schedule = Schedule.objects.get(pk=subscription.schedule)
             # get the messageset length for frequency
             messageset = MessageSet.objects.get(pk=subscription.messageset_id)
             subscription.metadata["frequency"] = \
                 str(len(messageset["messages"]))
             # Build the schedule POST create object
-            schedule = {
+            schedule_data = {
                 "subscriptionId": subscription_id,
                 "frequency": subscription.metadata["frequency"],
                 "sendCounter": subscription.next_sequence_number,
-                "cronDefinition": self.schedule_to_cron(csschedule),
+                "cronDefinition": self.schedule_to_cron(cs_schedule),
                 "endpoint": "%s/subscriptions/%s/send" % (  # TODO ?
                     settings.CONTROL_URL, subscription_id)
             }
-            result = scheduler.create_schedule(schedule)  # TODO
+            result = requests.post(
+                "%s/scheduler/" % settings.SCHEDULER_URL,  # TODO url
+                headers={'Content-Type': 'application/json'},
+                data=json.dumps(schedule_data),
+                auth=(settings.SCHEDULER_USERNAME,
+                      settings.SCHEDULER_PASSWORD),  # TODO Different Auth?
+                verify=False
+            )
             l.info("Created schedule <%s> on scheduler for sub <%s>" % (
                 result["id"], subscription_id))
             subscription.metadata["scheduler_schedule_id"] = result["id"]
@@ -124,14 +123,14 @@ class Create_Message(Task):
                     }
                 }
                 result = requests.post(
-                    "%s/outbound/" % settings.MESSAGESTORE_URL,  # url
+                    "%s/outbound/" % settings.MESSAGESTORE_URL,  # TODO url
                     headers={'Content-Type': 'application/json'},
                     data=json.dumps(outbound_data),
                     auth=(settings.MESSAGESTORE_USERNAME,
                           settings.MESSAGESTORE_PASSWORD),  # TODO Different Auth?
                     verify=False
                 )
-                return "New message created <%s>" % result.data["id"]  # TODO
+                return "New message created <%s>" % result["id"]  # TODO
             return "No message found for messageset <%s>, \
                     sequence_number <%s>, lang <%s>" % (
                 messageset_id, sequence_number, lang, )
