7e34ae49fcad295028a4c691fe59a1e0a4a03a35
gsvr <george@westerncapelabs.com>
2016-03-01 14:55:56 +0200

    basic tests

---
+++ contentstore/tests.py
@@ -0,0 +1,83 @@
+import json
+
+from django.test import TestCase
+from django.contrib.auth.models import User
+
+from rest_framework import status
+from rest_framework.test import APIClient
+from rest_framework.authtoken.models import Token
+
+from .models import Schedule, MessageSet
+
+
+class APITestCase(TestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+
+
+class AuthenticatedAPITestCase(APITestCase):
+
+    def make_schedule(self):
+        # Create hourly schedule
+        schedule_data = {
+            'hour': 1
+        }
+        return Schedule.objects.create(**schedule_data)
+
+    def setUp(self):
+        super(AuthenticatedAPITestCase, self).setUp()
+
+        self.username = 'testuser'
+        self.password = 'testpass'
+        self.user = User.objects.create_user(self.username,
+                                             'testuser@example.com',
+                                             self.password)
+        token = Token.objects.create(user=self.user)
+        self.token = token.key
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token)
+
+
+class TestLogin(AuthenticatedAPITestCase):
+
+    def test_login(self):
+        # Setup
+        post_auth = {"username": "testuser",
+                     "password": "testpass"}
+        # Execute
+        request = self.client.post(
+            '/api/token-auth/', post_auth)
+        token = request.data.get('token', None)
+        # Check
+        self.assertIsNotNone(
+            token, "Could not receive authentication token on login post.")
+        self.assertEqual(
+            request.status_code, 200,
+            "Status code on /api/token-auth was %s (should be 200)."
+            % request.status_code)
+
+
+class TestContentStoreApi(AuthenticatedAPITestCase):
+
+    def test_create_messageset(self):
+        # Setup
+        existing_schedule = self.make_schedule()
+        messageset_data = {
+            'short_name': 'messageset_one',
+            'notes': None,
+            'next_set': None,
+            'default_schedule': existing_schedule.id
+        }
+        # Execute
+        response = self.client.post('/api/v1/messageset/',
+                                    json.dumps(messageset_data),
+                                    content_type='application/json')
+        # Check
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        d = MessageSet.objects.last()
+        self.assertIsNotNone(d.id)
+        self.assertEqual(d.short_name, 'messageset_one')
+        self.assertEqual(d.notes, None)
+        self.assertEqual(d.next_set, None)
+        self.assertEqual(d.default_schedule, existing_schedule)
+        self.assertEqual(d.content_type, 'text')
