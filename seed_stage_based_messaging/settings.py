d83f8c844201e9cc158665a087ca2d1b1dac0a89
Mike Jones <mike@westerncapelabs.com>
2016-03-07 18:09:00 +0200

    Update staticfiles root

--- seed_stage_based_messaging/settings.py
+++ seed_stage_based_messaging/settings.py
@@ -102,7 +102,7 @@ STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
 )
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 # TEMPLATE_CONTEXT_PROCESSORS = (
