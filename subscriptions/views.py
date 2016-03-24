e2dad83be187ce47e7a6318e2affaf9cd8aa010d
Mike Jones <mike@westerncapelabs.com>
2016-03-24 15:49:29 +0200

    Remove unused permissions

--- subscriptions/views.py
+++ subscriptions/views.py
@@ -1,5 +1,5 @@
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.core.exceptions import ObjectDoesNotExist
