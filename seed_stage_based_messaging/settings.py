00c511dfa0c7d7887226bd654a687b3e341e98ce
Mike Jones <mike@westerncapelabs.com>
2016-07-01 09:13:08 +0200

    Fix broken ENVVAR setting - thanks @gsvr

--- seed_stage_based_messaging/settings.py
+++ seed_stage_based_messaging/settings.py
@@ -143,7 +143,7 @@ REST_FRAMEWORK = {
 CELERY_RESULT_BACKEND = 'djcelery.backends.database:DatabaseBackend'
 CELERYBEAT_SCHEDULER = 'djcelery.schedulers.DatabaseScheduler'
 
-BROKER_URL = os.environ.get('RABBITMQ_URL', 'redis://localhost:6379/0')
+BROKER_URL = os.environ.get('BROKER_URL', 'redis://localhost:6379/0')
 
 CELERY_DEFAULT_QUEUE = 'seed_stage_based_messaging'
 CELERY_QUEUES = (
