97215ec76ac6b5d6876284481b0ab1a5bc9998ee
Jason Paidoussi <jason@paidoussi.net>
2017-02-03 08:14:40 +0200

    Removed unused import

--- subscriptions/views.py
+++ subscriptions/views.py
@@ -3,7 +3,6 @@ from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
-from django.core.exceptions import ObjectDoesNotExist
 from django.contrib.auth.models import User
 import django_filters
 
