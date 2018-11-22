7bcf6a94d3afafdcc73955ec7b3007ab90e57e1e
Erik Harding <erik@praekelt.org>
2018-11-22 16:54:11 +0200

    add on_failure explanation

--- subscriptions/tasks.py
+++ subscriptions/tasks.py
@@ -97,6 +97,9 @@ class BaseSendMessage(Task):
         """
 
     def on_failure(self, exc, task_id, args, kwargs, einfo):
+        # This function only gets called once all retries have failed, not with
+        # each retry, this was tested in real life but doesn't work with unit
+        # tests when CELERY_ALWAYS_EAGER is True
         if isinstance(args[0], dict):
             subscription_id = args[0]["subscription_id"]
         else:
